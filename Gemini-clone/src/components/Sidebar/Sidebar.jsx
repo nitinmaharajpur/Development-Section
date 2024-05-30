90e5d775d0cffb3270a29748a5a7e96d8d9560b6
Nitin Kumar <nitimaharajpur@gmail.com>
2024-05-31 00:57:07 +0530

    Today I completed whole project

--- Gemini-clone/src/components/Sidebar/Sidebar.jsx
+++ Gemini-clone/src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useState } from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
 const Sidebar = () => {
     const [extended, setextended] = useState(false);
+    const { onSent, prevPrompts, setRecentPrompt,newChat } = useContext(Context)
+
+    const loadPrompt=async(prompt)=>{
+        setRecentPrompt(prompt)
+        await onSent(prompt)
+    }
     return (
         <div className='sidebar'>
             <div className="top">
-                <img onClick={()=>setextended(prev=>!prev)} className='menu' src={assets.menu_icon} alt="" />
-                <div className="new-chat">
+                <img onClick={() => setextended(prev => !prev)} className='menu' src={assets.menu_icon} alt="" />
+                <div onClick={()=>newChat()} className="new-chat">
                     <img src={assets.plus_icon} alt="" />
                     {extended ? <p>New Chat</p> : null}
                 </div>
                 {extended ? <div className="recent">
                     <p className="recent-title">Recent</p>
-                    <div className="recent-entry">
-                        <img src={assets.message_icon} alt="" />
-                        <p>What is react ...</p>
-                    </div>
+                    {prevPrompts.map((item, index) => {
+                        return (
+                            <div onClick={()=>loadPrompt(item)} className="recent-entry">
+                                <img src={assets.message_icon} alt="" />
+                                <p>{item.slice(0,18)}</p>
+                            </div>
+                        )
+                    })}
+
                 </div> : null}
 
             </div>
