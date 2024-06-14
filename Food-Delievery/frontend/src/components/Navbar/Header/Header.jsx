285caf501f157838f8827bf24c2f61fd53c3979e
Nitin Kumar <nitimaharajpur@gmail.com>
2024-06-14 08:09:26 +0530

    Restarting Development after interview

--- Food-Delievery/frontend/src/components/Navbar/Header/Header.jsx
+++ Food-Delievery/frontend/src/components/Navbar/Header/Header.jsx
@@ -3,7 +3,11 @@ import './Header.css'
 const Header = () => {
   return (
     <div className="header">
-      
+      <div className="header-contents">
+        <h2>Order your favourite food header</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint nulla repudiandae sit. Sapiente tempore nihil eaque amet fuga aspernatur sint sunt consequuntur quis? Quam rerum, non repudiandae veritatis, doloribus, impedit maiores repellat veniam libero esse ipsa ipsam eveniet odio omnis.</p>
+        <button>View Menu</button>
+      </div>
     </div>
   )
 }
